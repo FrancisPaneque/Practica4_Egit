371313256ba45861f53fcd12e8e1ec9fd2297c24
Francis <fran.pnq@gmail.com>
2023-01-29 13:43:41 +0100

    Hacemos un merge entre el main y las ramas 1 y 2

--- src/GeneradorDeNumerosAleatorios.java
+++ src/GeneradorDeNumerosAleatorios.java
@@ -3,9 +3,9 @@ public class GeneradorDeNumerosAleatorios {
 
   public static void main(String[] args) {
     
-    int[] numeros = new int[20];
+    int[] numeros = new int[30];
     
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < 30; i++) {
       
       numeros[i] = (int)(Math.random() * (10 - 1 + 1 ) + 1);
       
