83856b707673fc89af8daca21969b3a4835842a2
Francis <fran.pnq@gmail.com>
2023-01-29 13:34:52 +0100

    Añadimos un contador del numero de veces que se repte un numero de array

--- src/GeneradorDeNumerosAleatorios.java
+++ src/GeneradorDeNumerosAleatorios.java
@@ -4,16 +4,23 @@ public class GeneradorDeNumerosAleatorios {
   public static void main(String[] args) {
     
     int[] numeros = new int[20];
+    int[] repetidos = new int[20];
     
     for(int i = 0; i < 20; i++) {
       
       numeros[i] = (int)(Math.random() * (10 - 1 + 1 ) + 1);
-      
+      for (int j = 0; j < 20; i++) {
+        if (repetidos[j] == numeros[i]) {
+          repetidos[j]++;
+        }
+      }
     }
     
     for (int i = 0; i < numeros.length; i++) {
       
       System.out.print(numeros[i] + " ");
+      
+      
       
     }
 
